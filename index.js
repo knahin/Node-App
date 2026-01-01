9a58dae33dbae956944d84dbfee52d80e41e18c3
Ubuntu <ubuntu@ip-172-31-1-162.ap-south-1.compute.internal>
2026-01-01 13:52:18 +0000

    Add some line

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World! Express app is running by Knahin23 EC2 machine🚀");
+  res.send("Hello World! Express app is running by Knahin23 EC2 machine just try to make autmaiton for this application🚀");
 });
 
 app.listen(PORT, () => {
